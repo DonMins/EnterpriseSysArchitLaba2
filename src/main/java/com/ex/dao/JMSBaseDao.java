67fc275146e30ec5908a4c6c9405803139547dab
PashinaT <pashinatania@mail.ru>
2019-10-25 13:04:00 +0400

    JMSBase создана таблица

---
+++ src/main/java/com/ex/dao/JMSBaseDao.java
@@ -0,0 +1,13 @@
+package com.ex.dao;
+
+
+import com.ex.entity.JMSBase;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface JMSBaseDao extends JpaRepository<JMSBase, Integer> {
+
+    @Query("select u from JMSBase u where u.users.username=:username")
+    JMSBase findByUsername(@Param("username")String username);
+}
---
+++ src/main/java/com/ex/entity/JMSBase.java
@@ -0,0 +1,46 @@
+package com.ex.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.io.Serializable;
+
+/**
+ * A simple JavaBean domain object representing the JMS structure
+ *
+
+ */
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "jms")
+
+public class JMSBase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(name = "message",length = 65535)
+    private String message;
+
+    @OneToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name = "idUser")
+    private User users;
+
+
+
+    public JMSBase(User user, String message) {
+        this.message = message;
+        this.users = user;
+    }
+}
